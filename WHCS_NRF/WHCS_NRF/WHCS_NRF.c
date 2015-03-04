a1fcf76b5fb15ad4212f08414a66479cea85565b
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-03-04 13:48:36 -0500

    Created NRF Library Project WHCS_NRF

---
+++ WHCS_NRF/WHCS_NRF/WHCS_NRF.c
@@ -0,0 +1,15 @@
+/*
+ * WHCS_NRF.c
+ *
+ * Created: 3/4/2015 1:46:59 PM
+ *  Author: Jimmy
+ */ 
+
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}
