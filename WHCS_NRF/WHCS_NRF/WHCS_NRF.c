ae1c14d25455c7bd709e9a666a2d04c9d25b8c8f
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-03-04 13:56:29 -0500

    Began defining default values

--- WHCS_NRF/WHCS_NRF/WHCS_NRF.c
+++ WHCS_NRF/WHCS_NRF/WHCS_NRF.c
@@ -6,6 +6,21 @@
  */ 
 
 #include <avr/io.h>
+#include "nRF24L01.h"
+
+//Default length for the NRF Payload
+#ifndef DEF_PAYLOAD_SIZE
+#define DEF_PAYLOAD_SIZE 5
+#endif
+
+//Default channel used by the NRF, each step is 1 MHz starting at 2.4 GHZ
+#ifndef DEF_CHANNEL
+#define DEF_CHANNEL 0x36
+#endif
+
+#ifndef DEF_RETRIES
+#define DEF_RETRIES 15
+#endif
 
 int function(void)
 {
